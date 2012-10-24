344b0c4e5efc68ad5b7e6b522309175569d59012
Michael Hewner <hewner@gatech.edu>
2012-10-24 15:39:48 -0400

    Preliminary support for parson's problems

---
+++ modules/gatech/parsons/parsons.py
@@ -0,0 +1,108 @@
+# Copyright (C) 2012  Michael Hewner
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+__author__ = 'hewner'
+
+from docutils import nodes
+from docutils.parsers.rst import directives
+from docutils.parsers.rst import Directive
+
+def setup(app):
+    app.add_directive('parsonsprob',ParsonsProblem)
+
+    app.add_stylesheet('js-parsons/parsons.css')
+    app.add_stylesheet('js-parsons/lib/prettify.css')
+
+    # includes parsons specific javascript headers
+    # parsons-noconflict reverts underscore and
+    # jquery to their original versions
+    app.add_javascript('js-parsons/lib/jquery.min.js')
+    app.add_javascript('js-parsons/lib/jquery-ui.min.js')
+    app.add_javascript('js-parsons/lib/prettify.js')
+    app.add_javascript('js-parsons/lib/underscore-min.js')
+    app.add_javascript('js-parsons/lib/lis.js')
+    app.add_javascript('js-parsons/parsons.js')
+    app.add_javascript('js-parsons/parsons-noconflict.js')
+
+
+class ParsonsProblem(Directive):
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = False
+    option_spec = {}
+    has_content = True
+
+    def run(self):
+        """
+        TODO: Put some example here
+        """
+
+
+        template_values = {}
+        template_values['unique_id'] = self.lineno
+        template_values['code'] = "\n".join(self.content)
+        TEMPLATE = '''
+        <div>
+        <div id="parsons-orig-%(unique_id)s" style="display:none;">%(code)s</div>
+        <div id="parsons-sortableTrash-%(unique_id)s" class="sortable-code"></div>
+        <div id="parsons-sortableCode-%(unique_id)s" class="sortable-code"></div>
+	<div style="clear:left;"></div>
+        <a href="#" id="newInstanceLink-%(unique_id)s">Reset</a>
+        <a href="#" id="feedbackLink-%(unique_id)s">Get feedback</a>
+        </div>
+
+    <script>
+		var initial = 'def traverse_in_order(binary_node):\\n' +
+                  '  if binary_node:\\n' +
+					        '    foo\\n' +
+                  '  foo-1\\n';
+		var parson;
+
+	function displayErrors(fb) {
+	    if(fb.errors.length > 0) {
+                alert(fb.errors[0]);
+	    }
+	} 
+
+        $pjQ(document).ready(function(){
+            var pp_%(unique_id)s = new ParsonsWidget({
+                'sortableId': 'parsons-sortableCode-%(unique_id)s',
+		'trashId': 'parsons-sortableTrash-%(unique_id)s',
+                'max_wrong_lines': 1,
+                'feedback_cb' : displayErrors
+            });
+        pp_%(unique_id)s.init($pjQ("#parsons-orig-%(unique_id)s").text());
+	pp_%(unique_id)s.shuffleLines();
+            $pjQ("#newInstanceLink-%(unique_id)s").click(function(event){
+                event.preventDefault();
+                pp_%(unique_id)s.shuffleLines();
+            });
+            $pjQ("#feedbackLink-%(unique_id)s").click(function(event){
+                event.preventDefault();
+                pp_%(unique_id)s.getFeedback();
+            });
+            
+        });
+    </script>
+
+'''
+
+
+        self.assert_has_content()
+        return [nodes.raw('',TEMPLATE % template_values, format='html')]
+
+
+
+
