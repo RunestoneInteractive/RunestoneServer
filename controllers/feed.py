ca4678e9b5ac948049b4b21224031c62fa723ae9
Brad Miller <bonelake@gmail.com>
2013-01-11 15:10:15 -0600

    Add modification time to feed info.

--- controllers/feed.py
+++ controllers/feed.py
@@ -1,4 +1,5 @@
 import os, os.path
+import time
 
 def everyday():
     def get_posts(args, dname, flist):
@@ -11,18 +12,23 @@ def everyday():
         lname = lname.replace('applications','')
         for f in flist:
             if ".rst" in f:
-                ttext = open("%s/%s"%(dname,f)).readline()[:-1]
-            else:
-                ttext=""
-            f = f.replace(".rst",".html")
-            args.append(dict(title=ttext,
-                             link="http://interactivepython.org%s/%s" %(lname,f),
-                             description=""))
+                efile = open("%s/%s"%(dname,f))
+                ttext = efile.readline()[:-1]
+                efile.close()
+                stime = os.path.getmtime("%s/%s"%(dname,f))
+                mtime = time.ctime(stime)
+                f = f.replace(".rst",".html")
+                args.append(dict(title=ttext,
+                                 link="http://interactivepython.org%s/%s" %(lname,f),
+                                 description="",
+                                 created_on=mtime,
+                                 sort_time=stime
+                                 ))
     entry_list = []
-
     os.path.walk("applications/%s/everyday/2013"%request.application,get_posts,entry_list)
+    entry_list.sort(key=lambda x: x['sort_time'])
     return dict(title="Everyday Python",
-                link = "http://interactivepython.org/courselib/feed/everyday",
+                link = "http://interactivepython.org/courselib/feed/everyday.rss",
                 description="Everyday Python, Lessons in Python programming",
                 entries=entry_list
         )
