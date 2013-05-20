af60d0ec648b50039382eac4421bddfdf3ec6eae
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-05-20 15:34:38 -0500

    add a disqus comment box directive. Has only been registered in conf.py for the devcourse

--- devcourse/conf.py
+++ devcourse/conf.py
@@ -25,7 +25,7 @@ sys.path.insert(0, os.path.abspath('../modules'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.mathjax','luther.sphinx.video','luther.sphinx.codelens','luther.sphinx.activecode', 'luther.sphinx.assess', 'luther.sphinx.animation','luther.sphinx.meta','gatech.parsons']
+extensions = ['sphinx.ext.mathjax','luther.sphinx.video','luther.sphinx.disqus','luther.sphinx.codelens','luther.sphinx.activecode', 'luther.sphinx.assess', 'luther.sphinx.animation','luther.sphinx.meta','gatech.parsons']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['../source/_templates']
