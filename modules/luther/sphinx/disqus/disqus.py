14e4d5c24e068e07ebff895f57d946ba67830a40
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-05-21 16:28:10 -0500

    change Disqus directive to dynamically load based on identifier. This allows multiple comment boxes to be present on a page

--- modules/luther/sphinx/disqus/disqus.py
+++ modules/luther/sphinx/disqus/disqus.py
@@ -24,23 +24,38 @@ def setup(app):
     app.add_directive('disqus', Disqus)
 
 CODE = """\
-<div id="disqus_thread"></div>
 <script type="text/javascript">
-    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-    var disqus_shortname = '%(shortname)s'; // required: replace example with your forum shortname
-    var disqus_developer = 1;
-    var disqus_identifier = '%(identifier)s';
-    /* * * DON'T EDIT BELOW THIS LINE * * */
-    if (disqus_identifier.indexOf("index") < 0) {   // dont show the comments index
-        (function() {
+    function %(identifier)s(source) { 
+        if (window.DISQUS) {
+
+            jQuery('#disqus_thread').insertAfter(source); //append the HTML after the link
+
+            //if Disqus exists, call it's reset method with new parameters
+            DISQUS.reset({
+                reload: true,
+                config: function () {
+                    this.page.identifier = '%(identifier)s';
+                    this.page.url = 'http://www.%(identifier)s.com/#!';
+                }
+            });
+
+        } else {
+
+            //insert a wrapper in HTML after the relevant "show comments" link
+            jQuery('<div id="disqus_thread"></div>').insertAfter(source);
+            disqus_shortname = '%(shortname)s';    
+            disqus_identifier = '%(identifier)s'; //set the identifier argument
+            disqus_url = 'http://www.%(identifier)s.com/#!'; //set the permalink argument
+
+            //append the Disqus embed script to HTML
             var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
             dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-        })();
+            jQuery('head').append(dsq);
+
+        }
     }
 </script>
-<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+<a href="javascript:void(0)" onclick="%(identifier)s(this);">Show Comments</a>
 """
 
 class Disqus(Directive):
@@ -49,7 +64,7 @@ class Disqus(Directive):
     final_argument_whitespace = True
     has_content = False
     option_spec = {'shortname':directives.unchanged_required,
-                   'identifier':directives.unchanged_required
+                   'identifier':directives.unchanged_required,
                   }
 
 
