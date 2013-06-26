8616b0c703242673fc441188c14a475c270f73c8
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-06-26 14:37:24 -0500

    tweak RSI logo layout; make it link to RSI homepage

--- pythonds/conf.py
+++ pythonds/conf.py
@@ -159,7 +159,7 @@ html_short_title ='Problem Solving with Algorithms and Data Structures'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "../source/_static/logo_small.png"
+#html_logo = "../source/_static/logo_small.png"
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
