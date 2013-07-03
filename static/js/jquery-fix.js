66762ce911cd5ead837d7ac981da508b4f033470
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-07-03 14:58:28 -0500

    switch web2py-generated views to use the new jquery version

---
+++ static/js/jquery-fix.js
@@ -0,0 +1,4 @@
+// The Bootstrap-based Sphinx theme uses $jqTheme
+// Setting $ is needed to override the (old!) version of jQuery packaged with Sphinx
+var $jqTheme = jQuery.noConflict();
+var $ = jQuery.noConflict();
