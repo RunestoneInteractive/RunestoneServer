345dc8df35c4665742b543e6380d21582b418cb7
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-07-09 15:08:56 -0500

    initial add of register module

---
+++ test/run_tests.py
@@ -0,0 +1,16 @@
+__author__ = 'isaacdontjelindell'
+
+
+from register import LocalUser
+
+
+def main():
+    user = LocalUser()
+
+    user.register()
+
+    a = input("I'm stalling...")
+
+
+
+main()
