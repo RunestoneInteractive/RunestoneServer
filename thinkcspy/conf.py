6a389a42374ee220142d80009c1af25967b2b4cc
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-07-12 08:54:36 -0500

    bump copyright year

--- thinkcspy/conf.py
+++ thinkcspy/conf.py
@@ -41,7 +41,7 @@ master_doc = 'toc'
 
 # General information about the project.
 project = u'How to Think Like a Computer Scientist'
-copyright = u'2012, Brad Miller, David Ranum'
+copyright = u'2013 Brad Miller, David Ranum'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
