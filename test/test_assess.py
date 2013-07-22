1c702460b7e49e0f2c31ba5cae5afda5c5405561
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-07-22 14:57:49 -0500

    more codelens tests

--- test/test_assess.py
+++ test/test_assess.py
@@ -29,7 +29,8 @@ class AssessTests(unittest.TestCase):
 
         # check that feedback is correct
         feedback = question.find_element_by_id('question1_1_feedback').text
-        self.assertTrue("Correct!!" in feedback, "MCMF correct answer not graded correctly!")
+        self.assertTrue("Correct!!" in feedback,
+                        "MCMF correct answer not graded correctly!")
 
         # select an incorrect answer
         question.find_element_by_id('question1_1_opt_b').click()
@@ -39,7 +40,8 @@ class AssessTests(unittest.TestCase):
 
         # check that feedback is incorrect
         feedback = question.find_element_by_id('question1_1_feedback').text
-        self.assertTrue("Incorrect." in feedback, "MCMF incorrect answer not graded correctly!")
+        self.assertTrue("Incorrect." in feedback,
+                        "MCMF incorrect answer not graded correctly!")
 
 
     def test_MCMA(self):
@@ -58,7 +60,8 @@ class AssessTests(unittest.TestCase):
 
         # check that feedback is correct
         feedback = question.find_element_by_id('question1_2_feedback').text
-        self.assertTrue("Correct!" in feedback, "MCMA correct answers not graded correctly!")
+        self.assertTrue("Correct!" in feedback,
+                        "MCMA correct answers not graded correctly!")
 
         # select a wrong answer
         question.find_element_by_id('question1_2_opt_c').click()
@@ -68,7 +71,8 @@ class AssessTests(unittest.TestCase):
 
         # check that feedback is incorrect
         feedback = question.find_element_by_id('question1_2_feedback').text
-        self.assertTrue("Incorrect." in feedback, "MCMA incorrect answer not graded correctly!")
+        self.assertTrue("Incorrect." in feedback,
+                        "MCMA incorrect answer not graded correctly!")
 
     def test_FIB(self):
         self.driver.get(self.host + '/runestone/static/overview/overview.html')
@@ -84,7 +88,8 @@ class AssessTests(unittest.TestCase):
 
         # check that feedback is correct
         feedback = question.find_element_by_id('baseconvert1_feedback').text
-        self.assertTrue('You are Correct' in feedback, "FIB correct answer not graded correctly!")
+        self.assertTrue('You are Correct' in feedback,
+                        "FIB correct answer not graded correctly!")
 
         # input the wrong answer
         question.find_element_by_id('baseconvert1_ans1').send_keys('50')
@@ -95,7 +100,8 @@ class AssessTests(unittest.TestCase):
 
         # check that feedback is incorrect
         feedback = question.find_element_by_id('baseconvert1_feedback').text
-        self.assertTrue('Incorrect.' in feedback, "FIB incorrect answer not graded correctly!")
+        self.assertTrue('Incorrect.' in feedback,
+                        "FIB incorrect answer not graded correctly!")
 
 
     def test_codelens(self):
@@ -115,4 +121,40 @@ class AssessTests(unittest.TestCase):
         expected_output = 'My first program adds two numbers, 2 and 3:\n5\n'
         self.assertTrue(expected_output in output, "Codelens output is incorrect!")
 
+        # test inline "Check Your Understanding" question
+        question = self.driver.find_element_by_id('codelens_question')
+        forward_button = question.find_element_by_id('jmpStepFwd')
+
+
+        modal = self.driver.find_element_by_id('codelens_question_modal')
+        disp = modal.value_of_css_property('display')
+        self.assertTrue(disp == 'none',
+                        "'Check Your Understanding modal is visible when it shouldn't be!")
+
+        forward_button.click()
+        forward_button.click()
+        forward_button.click()
+
+        modal = self.driver.find_element_by_id('codelens_question_modal')
+        disp = modal.value_of_css_property('display')
+        self.assertTrue(disp == 'block')
+
+        self.assertTrue("What is the value of tot" in modal.text)
+
+        modal_input = modal.find_element_by_id('codelens_question_textbox')
+        modal_input.send_keys('0')
+
+        check_button = modal.find_element_by_id('codelens_question_tracecheck')
+        check_button.click()
+
+        feedback_text = modal.find_element_by_id('codelens_question_feedbacktext').text
+        self.assertTrue('Correct' in feedback_text,
+                        "The correct answer feedback for 'Check Your Understanding' question is wrong!")
+
+        modal_input.send_keys('5')
+        check_button.click()
+
+        feedback_text = modal.find_element_by_id('codelens_question_feedbacktext').text
+        self.assertTrue('Use the global variables box to look' in feedback_text,
+                        "The incorrect answer feedback for 'Check Your Understanding' question is wrong!")
 
