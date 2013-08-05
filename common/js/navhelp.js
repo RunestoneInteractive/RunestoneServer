a5e4c92894df1a1597998b32b5a767246b511031
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-08-05 09:05:06 -0500

    remove introjs as it doesn't work for fixed elements, like the bootstrap navbar

--- common/js/navhelp.js
+++ common/js/navhelp.js
@@ -7,18 +7,6 @@
 var is_setup;
 
 function setup() {
-    var ac_block = $("#codeexample1");
-    ac_block.attr('data-intro', 'ActiveCode blocks allow you to run Python programs right in the textbook');
-    ac_block.attr('data-step', '1');
-
-    var ac_code = $("codeexample1_code_div");
-    ac_code.attr('data-intro', 'Write and edit code here');
-    ac_code.attr('data-step', '2');
-
-
-    var ac_run = $("codeexample1_runb");
-    ac_run.attr('data-intro', 'Click here to execute the code');
-    ac_run.attr('data-step', '3');
 }
 
 
