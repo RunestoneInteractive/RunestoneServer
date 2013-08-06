13cb835ca47ee999882f708b319eae75dac910d0
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-08-06 16:01:54 -0500

    add the theme to the newproject folder to make for easy customization

--- newproject_copy_me/conf.py
+++ newproject_copy_me/conf.py
@@ -99,7 +99,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["../source/_templates/plugin_layouts"]
+html_theme_path = ["_templates"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
