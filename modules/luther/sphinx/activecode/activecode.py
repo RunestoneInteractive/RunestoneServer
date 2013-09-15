ed4c8ed6f160b2975fbe3cf82cf25c46bd77fd1e
Brad Miller <bonelake@gmail.com>
2013-09-15 09:37:17 -0500

    Undo changes to activecode to get files from app engine.  Better to spend effort combining the files rather than making things complicated by relying on another server.

--- modules/luther/sphinx/activecode/activecode.py
+++ modules/luther/sphinx/activecode/activecode.py
@@ -22,21 +22,27 @@ from docutils.parsers.rst import Directive
 import json
 import os
 
+# try:
+#     import conf
+#     version = conf.version
+#     staticserver = conf.staticserver
+# except:
+#     version = '2.1.0'
+#     staticserver = 'runestonestatic.appspot.com'
+
 def setup(app):
-    version = '2.1.0'
-    staticserver = 'runestonestatic.appspot.com'
     app.add_directive('activecode',ActiveCode)
     app.add_directive('actex',ActiveExercise)
-    app.add_stylesheet('http://%s/static/%s/codemirror.css' % (staticserver,version))
-    app.add_stylesheet('http://%s/static/%s/activecode.css' % (staticserver,version))
+    app.add_stylesheet('codemirror.css')
+    app.add_stylesheet('activecode.css')
 
-    app.add_javascript('http://%s/static/%s/jquery.highlight.js' % (staticserver,version))
-    app.add_javascript('http://%s/static/%s/bookfuncs.js' % (staticserver, version))
-    app.add_javascript('http://%s/static/%s/codemirror.js' % (staticserver, version))
-    app.add_javascript('http://%s/static/%s/python.js' % (staticserver, version))
-    app.add_javascript('http://%s/static/%s/activecode.js' % (staticserver, version))
-    app.add_javascript('http://%s/static/%s/skulpt.js' % (staticserver, version))
-    app.add_javascript('http://%s/static/%s/builtin.js' % (staticserver, version))
+    app.add_javascript('jquery.highlight.js' )
+    app.add_javascript('bookfuncs.js' )
+    app.add_javascript('codemirror.js' )
+    app.add_javascript('python.js' )
+    app.add_javascript('activecode.js')
+    app.add_javascript('skulpt.js' )
+    app.add_javascript('builtin.js')
 
     app.add_node(ActivcodeNode, html=(visit_ac_node, depart_ac_node))
 
