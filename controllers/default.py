4460a1315a751d81400797b41efd2c4739fe4a77
Nick Reid <nickreid@nickreid.com>
2014-01-08 19:01:30 -0500

    added basic boilerplate view of form

--- controllers/default.py
+++ controllers/default.py
@@ -84,4 +84,14 @@ def about():
 def ack():
     return dict()
 
+def bio():
+    form = SQLFORM(db.user_biography)
+    if form.process().accepted:
+        response.flash = 'form accepted'
+    elif form.errors:
+        response.flash = 'form has errors'
+    else:
+        response.flash = 'please fill out the form'
+    return dict(form=form)
+
     
