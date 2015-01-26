a77fe1247df07ce24cc249aef949972bff90dcd4
Paul Resnick <presnick@umich.edu>
2015-01-25 20:54:34 -0500

    grading: replace null with nothing submitted
    Conflicts:
    	common/js/bookfuncs.js

--- common/js/bookfuncs.js
+++ common/js/bookfuncs.js
@@ -217,6 +217,10 @@ function createActiveCode(divid,suppliedSource,sid,language) {
                 });
 
         editor.setSize(null,250);
+        if (! suppliedSource ) {
+              suppliedSource = 'nothing submitted';
+        }
+        editor.setValue(suppliedSource);
 
 
         var myRun = function() {
