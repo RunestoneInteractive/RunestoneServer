41d8ed510973cee91d60a812964063682a97aec9
ericsonga <ericson@cc.gatech.edu>
2015-04-29 21:29:03 -0400

    New timed exam feature
    Added a new feature to allow for timed exams.  Modified the local
    storage of answers to multiple choice questions and fill in the blank
    questions to use the user id in front of the divid.

--- modules/luther/sphinx/assess/multiplechoice.py
+++ modules/luther/sphinx/assess/multiplechoice.py
@@ -302,7 +302,7 @@ class MChoiceRandomMF(Assessment):
         super(MChoiceRandomMF,self).run()
 
         res = ""
-        res = TEMPLATE_START % self.options
+        res = TEMPLATE_START % self.options #fill in % with options on the directive
         feedbackStr = "["
         currFeedback = ""
         # Add all of the possible answers
@@ -319,7 +319,7 @@ class MChoiceRandomMF(Assessment):
                 ansArr.append(k)
         for f in ansArr:
             t,flabel=f.split("_")
-            feedArray.append(flabel)
+            feedArray.append(flabel) #proces input array
 
         i=0
         for k in okeys:
@@ -327,15 +327,16 @@ class MChoiceRandomMF(Assessment):
                 answ=answ+self.options[ansArr[i]]+"*separator*"
                 feed=feed+self.options["feedback_"+feedArray[i]]+"*separator*"
                 self.options['opi']=i+1
-                res += OPTION % self.options
+                res += OPTION % self.options 
             i=i+1
 
-        # Store the Answer and Feedback arrays
+        # Store the Answer and Feedback arrays getting stuf answ array and feedback array and placing into options
         self.options['a']=answ
         self.options['f']=feed
 
-        op=self.options['correct']
+        op=self.options['correct']  #checks to see which is corect
 
+#maps correct answer to answer array
         if(op=='a'):
             index=0
         elif(op=='b'):
@@ -349,4 +350,87 @@ class MChoiceRandomMF(Assessment):
         self.options['corr']=self.options[ansArr[index]]
 
         res += TEMPLATE_END % self.options
-        return [nodes.raw('',res , format='html')]
+        return [nodes.raw('',res , format='html')] #return the array with the results in HTML Directive format stuff and spits out in HTML
+
+######################################################################################################
+#####Timed MChoiceMF
+
+class timedMChoiceMF(Assessment):
+    required_arguments = 1
+    optional_arguments = 1
+    final_argument_whitespace = True
+    has_content = True
+    option_spec = {'answer_a':directives.unchanged,
+        'answer_b':directives.unchanged,
+        'answer_c':directives.unchanged,
+        'answer_d':directives.unchanged,
+        'answer_e':directives.unchanged,
+        'correct':directives.unchanged,
+        'feedback_a':directives.unchanged,
+        'feedback_b':directives.unchanged,
+        'feedback_c':directives.unchanged,
+        'feedback_d':directives.unchanged,
+        'feedback_e':directives.unchanged,
+        'iscode':directives.flag
+    }
+
+    def run(self):
+        """
+            process the multiplechoice directive and generate html for output.
+            :param self:
+            :return:
+            .. mcmfstorage:: qname
+            :iscode: boolean
+            :answer_a: possible answer  -- what follows _ is label
+            :answer_b: possible answer
+            ...
+            :answer_e: possible answer
+            :correct: leter of correct answer
+            :feedback_a: displayed if a is picked
+            :feedback_b: displayed if b is picked
+            :feedback_c: displayed if c is picked
+            :feedback_d: displayed if d is picked
+            :feedback_e: displayed if e is picked
+
+            Question text
+            ...
+            """
+        TEMPLATE_START = '''
+            <div id="%(divid)s" class="alert alert-warning">
+            '''
+
+        OPTION = '''
+            <input type="radio" name="group1" value="%(alabel)s" id="%(divid)s_opt_%(alabel)s" />
+            <label for= "%(divid)s_opt_%(alabel)s">  %(alabel)s) %(atext)s</label><br />
+	        <div id= "%(divid)s_eachFeedback_%(alabel)s"></div>
+            '''
+
+        TEMPLATE_END = '''
+
+            <script>
+		    populateArrays('%(divid)s','%(correct)s',%(feedback)s);
+            $(document).ready(function(){checkTimedRadio('%(divid)s');});
+            </script>
+            
+ 
+            </form><br />
+            <div id="%(divid)s_feedback">
+            </div>
+            </div>
+            '''
+        super(timedMChoiceMF,self).run();
+
+        mcNode = MChoiceNode(self.options)
+        mcNode.template_start = TEMPLATE_START
+        mcNode.template_form_start = '''<div class="timeTip" title=" "><img src="../_static/clock.png" style="width:15px;height:15px"></div><form name="%(divid)s_form" method="get" action="" onsubmit="return false;">'''
+        mcNode.template_option = OPTION
+        mcNode.template_end = TEMPLATE_END
+
+        self.state.nested_parse(self.content, self.content_offset, mcNode)
+
+        return [mcNode]
+
+
+######################################################################
+
+
