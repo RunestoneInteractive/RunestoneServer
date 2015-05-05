dd7e52e3ae102ea96b79955b2253c1ed70e11d12
Brad Miller <bonelake@mac.com>
2015-05-05 16:51:51 +0200

    templates for conf and pavement.

--- books/modules/runestone/common/newproject_copy_me/pavement.py
+++ books/modules/runestone/common/newproject_copy_me/pavement.py
@@ -8,44 +8,43 @@ from sphinxcontrib import paverutils
 
 sys.path.append(os.getcwd())
 
-######## CHANGE THIS ##########
-project_name = "<project_name>"
-###############################
-
-master_url = 'http://127.0.0.1:8000'
+home_dir = os.getcwd()
+master_url = '%(master_url)s'
 master_app = 'runestone'
+serving_dir = "%(build_dir)s/%(project_name)s"
 
 options(
     sphinx = Bunch(docroot=".",),
 
     build = Bunch(
-        builddir="../static/"+project_name,
+        builddir="%(build_dir)s/%(project_name)s",
         sourcedir="_sources",
-        outdir="../static/"+project_name,
+        outdir="%(build_dir)s/%(project_name)s",
         confdir=".",
-        template_args={'course_id':project_name,
-                       'login_required':'false',
+        template_args={'course_id': '%(project_name)s',
+                       'login_required':'%(login_req)s',
                        'appname':master_app,
                        'loglevel':10,
                        'course_url':master_url }
     )
 )
 
-if project_name == "<project_name>":
-  print "Please edit pavement.py and give your project a name"
-  exit()
 
 @task
 @cmdopts([
     ('all','a','rebuild everything'),
     ('outputdir=', 'o', 'output static files here'),
     ('masterurl=', 'u', 'override the default master url'),
-    ('masterapp=', 'p', 'override the default master app')
+    ('masterapp=', 'p', 'override the default master app'),
 ])
 def build(options):
     if 'all' in options.build:
       options['force_all'] = True
       options['freshenv'] = True
+
+    # bi = sh('git describe --long',capture=True)[:-1]
+    # bi = bi.split('-')[0]
+    # options.build.template_args["build_info"] = bi
 
     if 'outputdir' in options.build:
         options.build.outdir = options.build.outputdir
