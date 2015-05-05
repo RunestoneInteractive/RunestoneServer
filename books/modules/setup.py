30ee8021eaf48cb6ff8765dcd0596080275746c9
Brad Miller <bonelake@mac.com>
2015-05-05 11:37:05 +0200

    move newproject_copy_me into the module

--- books/modules/setup.py
+++ books/modules/setup.py
@@ -1,16 +1,27 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt', 'r') as fh:
+    dependencies = [l.strip() for l in fh]
+
 setup(
     name='runestone',
     version='0.1dev',
     author = 'Brad Miller',
     author_email = 'bonelake@mac.com',
     packages= find_packages(),
+    install_requires=dependencies,
     include_package_data = True,
+    zip_safe = False,
     package_dir = {'runestone' : 'runestone'},
     package_data = { '' : ['js/*.js', 'css/*.css', '*.txt']},
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    scripts=['bin/runestone'],
-    data_files=[('common',['common/*'])],
+    # data_files=[('common',['runestone/common/*']),
+    #             ('project/template', ['newproject_copy_me/*'])
+    # ],
     long_description=open('README.txt').read(),
+    entry_points = {
+        'console_scripts': [
+            'runestone = runestone.__main__:main'
+        ]
+        }
 )
