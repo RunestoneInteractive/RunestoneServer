d326e51f6eea68f65b8a657263f8ccf697889a8a
Brad Miller <bonelake@mac.com>
2015-05-28 16:44:16 +0200

    Try using flask-restful with flask-cors et. al.

--- runestone/ajax/__init__.py
+++ runestone/ajax/__init__.py
@@ -7,15 +7,26 @@ from flask.ext.security import Security, MongoEngineUserDatastore, \
 from runestone import db, app
 from runestone.model import *
 from flask.ext.cors import cross_origin
+from flask_restful import Api, Resource
 
 ajax = Blueprint('ajax', __name__,
           template_folder='templates',static_folder='static')
 
+api = Api(app)
 
-@ajax.route('/ajax/page')
-@login_required
-@cross_origin(supports_credentials=True)
-def test():
-    return jsonify({'foo':'bar'})
+class HelloWorld(Resource):
+    @cross_origin(supports_credentials=True)
+    @login_required
+    def get(self):
+        return jsonify({'hello': 'world'})
+
+api.add_resource(HelloWorld, '/ajax/page')
+
+
+# @ajax.route('/ajax/page')
+# @login_required
+# @cross_origin(supports_credentials=True)
+# def test():
+#     return jsonify({'foo':'bar'})
 
 
