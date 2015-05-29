2d6f489c468f092a6984e563b67681b610ffa79a
Brad Miller <bonelake@mac.com>
2015-05-29 09:02:29 +0200

    Add logging, use Blueprint

--- runestone/ajax/__init__.py
+++ runestone/ajax/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'bmiller'
 from flask import Flask, Blueprint, render_template, jsonify, request
-
+import logging
 from flask.ext.security import Security, MongoEngineUserDatastore, \
     UserMixin, RoleMixin, login_required
 
@@ -12,12 +12,16 @@ from flask_restful import Api, Resource
 ajax = Blueprint('ajax', __name__,
           template_folder='templates',static_folder='static')
 
-api = Api(app)
+api = Api(ajax)
+
+logger = logging.getLogger(__name__)
+logger.debug("setup complete")
 
 class HelloWorld(Resource):
     @cross_origin(supports_credentials=True)
     @login_required
     def get(self):
+        logger.debug("here in get")
         return jsonify({'hello': 'world'})
 
 api.add_resource(HelloWorld, '/ajax/page')
