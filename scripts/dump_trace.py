e70a1be9b452e1b1cb107fc314662de1887fbda1
Brad Miller <bonelake@mac.com>
2015-08-07 09:05:15 -0500

    dump frames and locals

--- scripts/dump_trace.py
+++ scripts/dump_trace.py
@@ -3,10 +3,16 @@ __author__ = 'bmiller'
 
 import cPickle
 import sys
+import pprint
 
-sys.path.insert(0,'/Users/bmiller/Beta/web2py')
+sys.path.insert(0,'/home/bnmnetp/webapps/uwsgi_runestone/web2py')
 
 with open(sys.argv[1]) as f:
     trace_data = cPickle.load(f)
 
 print(trace_data['traceback'])
+pprint.pprint(trace_data['snapshot']['locals'])
+for i in trace_data['snapshot']['frames']:
+    print("============")
+    pprint.pprint(i)
+
