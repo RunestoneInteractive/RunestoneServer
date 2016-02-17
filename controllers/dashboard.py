df468e7a5753d3f9d65b2ceb63c6bde1e0c2fea7
mmoldavan <matt.moldavan@careerbuilder.com>
2016-02-17 00:53:03 -0500

    detailing dashboard design

--- controllers/dashboard.py
+++ controllers/dashboard.py
@@ -16,22 +16,134 @@ from paver.easy import sh
 
 # select acid, sid from code as T where timestamp = (select max(timestamp) from code where sid=T.sid and acid=T.acid);
 
-
 @auth.requires_login()
 def index():
     row = db(db.courses.id == auth.user.course_id).select(db.courses.course_name, db.courses.base_course).first()
     course = db(db.courses.id == auth.user.course_id).select().first()
-
-    return dict(course_name=auth.user.course_name)
+    questions = [
+        {
+        "id": "5",
+        "text": "CSP-4-4-4: Put the blocks below into the correct order to print a twist on a famous poem.",
+        "correctPercent": '15%',
+        "attemptPercent": '40%',
+        "missedPercent": '45%',
+        "neverPercent": '5%',
+        "correct":5,
+        "attempted":4,
+        "missed":9,
+        "never":2,
+        "completedStudents": '18'
+        },
+                {
+        "id": "6",
+        "text": "CSP-4-2-3: Given the following code segment, what will be printed?",
+        "correctPercent": '35%',
+        "attemptPercent": '20%',
+        "missedPercent": '45%',
+        "correct":7,
+        "attempted":3,
+        "missed":8,
+        "never":2,
+        "completedStudents": '18'
+        },
+                {
+        "id": "7",
+        "text": "CSP-4-2-1: What will be printed when the following executes?",
+        "correctPercent": '40%',
+        "attemptPercent": '40%',
+        "missedPercent": '20%',
+        "correct":8,
+        "attempted":3,
+        "missed":4,
+        "never":5,
+        "completedStudents": '15'
+        },
+                {
+        "id": "8",
+        "text": "CSP-4-1-2: What will be printed when the following executes?",
+        "correctPercent": '55%',
+        "attemptPercent": '30%',
+        "missedPercent": '15%',
+        "correct":10,
+        "attempted":4,
+        "missed":3,
+        "never":3,
+        "completedStudents": '17'
+        },
+                {
+        "id": "9",
+        "text": "CSP-4-1-1: : Given the following code segment, what will be printed?",
+        "correctPercent": '60%',
+        "attemptPercent": '30%',
+        "missedPercent": '10%',
+        "correct":14,
+        "attempted":3,
+        "missed":1,
+        "never":2,
+        "completedStudents": '18'
+        }
+    ]
+    sections = [
+        {
+            "id": "5",
+            "text": "Assign a Name to a String",
+            "readPercent": '75%',
+            "startedPercent": '20%',
+            "unreadPercent": '5%',
+        },        {
+            "id": "6",
+            "text": "Strings are Objects",
+            "readPercent": '60%',
+            "startedPercent": '30%',
+            "unreadPercent": '10%',
+        },        {
+            "id": "7",
+            "text": "Strings are Immutable",
+            "readPercent": '25%',
+            "startedPercent": '40%',
+            "unreadPercent": '35%',
+        },        {
+            "id": "8",
+            "text": "Making a MadLib Story",
+            "readPercent": '15%',
+            "startedPercent": '10%',
+            "unreadPercent": '75%',
+        },        {
+            "id": "8",
+            "text": "Chapter 4 - Summary",
+            "readPercent": '45%',
+            "startedPercent": '10%',
+            "unreadPercent": '45%',
+        },        {
+            "id": "8",
+            "text": "Chapter 4 Exercises",
+            "readPercent": '5%',
+            "startedPercent": '10%',
+            "unreadPercent": '85%',
+        }
+    ]
+    return dict(course_name=auth.user.course_name, questions=questions, sections=sections)
 
 @auth.requires_login()
 def studentreport():
     row = db(db.courses.id == auth.user.course_id).select(db.courses.course_name, db.courses.base_course).first()
     course = db(db.courses.id == auth.user.course_id).select().first()
 
-    return dict(course_name=auth.user.course_name,student_name=auth.user.first_name)
+    chapters = [{
+        "text":"Chapter 1: What is this book about?",
+        "sections":[{
+            "text"
+        }]
+    }]
+    return dict(course_name=auth.user.course_name,student_name=auth.user.first_name + " " + auth.user.last_name, chapters=chapters)
 
 def studentprogress():
+    row = db(db.courses.id == auth.user.course_id).select(db.courses.course_name, db.courses.base_course).first()
+    course = db(db.courses.id == auth.user.course_id).select().first()
+
+    return dict(course_name=auth.user.course_name)
+
+def grades():
     row = db(db.courses.id == auth.user.course_id).select(db.courses.course_name, db.courses.base_course).first()
     course = db(db.courses.id == auth.user.course_id).select().first()
 
@@ -41,4 +153,24 @@ def exercisemetrics():
     row = db(db.courses.id == auth.user.course_id).select(db.courses.course_name, db.courses.base_course).first()
     course = db(db.courses.id == auth.user.course_id).select().first()
 
-    return dict(course_name=auth.user.course_name)
+    answers = [{
+        "student":"Bob Brown",
+        "answers":["4.0", "0", "2","4"]
+    },{
+        "student":"Nick Collans",
+        "answers":["2.0", "4.0", "2","4"]
+    },{
+        "student":"Tim Collans",
+        "answers":["8", "4", "",""]
+    },{
+        "student":"Xu Hung",
+        "answers":["8", "4", "", ""]
+    },{
+        "student":"Jack Jackson",
+        "answers":["5", "4", "", ""]
+    },{
+        "student":"Brittany Jones",
+        "answers":["4", "", "", ""]
+    }]
+
+    return dict(course_name=auth.user.course_name, answers=answers)
--- static/dashboard-charts.js
+++ static/dashboard-charts.js
@@ -1,213 +1,412 @@
-var margins = {
-    top: 12,
-    left: 75,
-    right: 24,
-    bottom: 24
-},
-legendPanel = {
-    width: 250
-},
-width = 500 - margins.left - margins.right - legendPanel.width,
-    height = (8 * 20) - margins.top - margins.bottom,
-    dataset = [{
-        data: [{
-            student: 'Bob Brown',
-            count: 4
-        }, {
-            student: 'Jack Jackson',
-            count: 10
-        }, {
-            student: 'Macy Millan',
-            count: 7
-        }, {
-            student: 'Sarah Smith',
-            count: 7
-        }, {
-            student: 'Xu Hung',
-            count: 4
-        }, {
-            student: 'Rachel Ru',
-            count: 8
-        }, {
-            student: 'John Hendrick',
-            count: 6
-        }, {
-            student: 'Nick Collans',
-            count: 5
-        }],
-        name: 'Sections Read'
-    }, {
-        data: [{
-            student: 'Bob Brown',
-            count: 11
-        }, {
-            student: 'Jack Jackson',
-            count: 13
-        }, {
-            student: 'Macy Millan',
-            count: 17
-        }, {
-            student: 'Sarah Smith',
-            count: 9
-        }, {
-            student: 'Xu Hung',
-            count: 4
-        }, {
-            student: 'Rachel Ru',
-            count: 17
-        }, {
-            student: 'John Hendrick',
-            count: 13
-        }, {
-            student: 'Nick Collans',
-            count: 12
-        }],
-        name: 'Exercises Correct'
-    }, {
-        data: [{
-            student: 'Bob Brown',
-            count: 3
-        }, {
-            student: 'Jack Jackson',
-            count: 6
-        }, {
-            student: 'Macy Millan',
-            count: 1
-        }, {
-            student: 'Sarah Smith',
-            count: 4
-        }, {
-            student: 'Xu Hung',
-            count: 0
-        }, {
-            student: 'Rachel Ru',
-            count: 2
-        }, {
-            student: 'John Hendrick',
-            count: 1
-        }, {
-            student: 'Nick Collans',
-            count: 3
-        }],
-        name: 'Exercises Missed'
+DashboardCharts = DashboardCharts();
+function DashboardCharts(){
+    return {
+        studentActivity: studentActivity,
+        exerciseAttempts: exerciseAttempts,
+        donutChart: donutChart
     }
 
-    ],
-    series = dataset.map(function (d) {
-        return d.name;
-    }),
-    dataset = dataset.map(function (d) {
-        return d.data.map(function (o, i) {
-            // Structure it so that your numeric
-            // axis (the stacked amount) is y
-            return {
-                y: o.count,
-                x: o.student
-            };
-        });
-    }),
-    stack = d3.layout.stack();
+    function studentActivity() {
+        var margins = {
+            top: 12,
+            left: 100,
+            right: 24,
+            bottom: 24
+        },
+        width = 400 - margins.left - margins.right,
+            height = (15 * 24) - margins.top - margins.bottom,
+            
+            dataset = [{
+                data: [{
+                    student: 'Bob Brown',
+                    count: 4
+                }, {
+                    student: 'Jack Jackson',
+                    count: 10
+                }, {
+                    student: 'Macy Millan',
+                    count: 7
+                }, {
+                    student: 'Sarah Smith',
+                    count: 7
+                }, {
+                    student: 'Xu Hung',
+                    count: 4
+                }, {
+                    student: 'Rachel Ru',
+                    count: 8
+                }, {
+                    student: 'John Hendrick',
+                    count: 6
+                }, {
+                    student: 'Nick Collans',
+                    count: 5
+                }, {
+                    student: 'Tim Collans',
+                    count: 2
+                }, {
+                    student: 'Gina Kidder',
+                    count: 7
+                }, {
+                    student: 'Hannah Nguyen',
+                    count: 3
+                }, {
+                    student: 'Eric Glazebrook',
+                    count: 11
+                }, {
+                    student: 'Tommy Ruz',
+                    count: 8
+                }, {
+                    student: 'Brittany Jones',
+                    count: 3
+                }, {
+                    student: 'Mary Salter',
+                    count: 4
+                }],
+                name: 'Sections Read'
+            }, {
+                data: [{
+                    student: 'Bob Brown',
+                    id: 'bbrown',
+                    count: 11
+                }, {
+                    student: 'Jack Jackson',
+                    id: 'bbrown',
+                    count: 13
+                }, {
+                    student: 'Macy Millan',
+                    id: 'bbrown',
+                    count: 17
+                }, {
+                    student: 'Sarah Smith',
+                    id: 'bbrown',
+                    count: 9
+                }, {
+                    student: 'Xu Hung',
+                    id: 'bbrown',
+                    count: 4
+                }, {
+                    student: 'Rachel Ru',
+                    id: 'bbrown',
+                    count: 17
+                }, {
+                    student: 'John Hendrick',
+                    id: 'bbrown',
+                    count: 13
+                }, {
+                    student: 'Nick Collans',
+                    id: 'bbrown',
+                    count: 12
+                }, {
+                    student: 'Tim Collans',
+                    count: 9
+                }, {
+                    student: 'Gina Kidder',
+                    count: 7
+                }, {
+                    student: 'Hannah Nguyen',
+                    count: 8
+                }, {
+                    student: 'Eric Glazebrook',
+                    count: 14
+                }, {
+                    student: 'Tommy Ruz',
+                    count: 16
+                }, {
+                    student: 'Brittany Jones',
+                    count: 2
+                }, {
+                    student: 'Mary Salter',
+                    count: 2
+                }],
+                name: 'Exercises Correct'
+            }, {
+                data: [{
+                    student: 'Bob Brown',
+                    count: 3
+                }, {
+                    student: 'Jack Jackson',
+                    count: 6
+                }, {
+                    student: 'Macy Millan',
+                    count: 1
+                }, {
+                    student: 'Sarah Smith',
+                    count: 4
+                }, {
+                    student: 'Xu Hung',
+                    count: 0
+                }, {
+                    student: 'Rachel Ru',
+                    count: 2
+                }, {
+                    student: 'John Hendrick',
+                    count: 1
+                }, {
+                    student: 'Nick Collans',
+                    count: 3
+                }, {
+                    student: 'Tim Collans',
+                    count: 5
+                }, {
+                    student: 'Gina Kidder',
+                    count: 8
+                }, {
+                    student: 'Hannah Nguyen',
+                    count: 9
+                }, {
+                    student: 'Eric Glazebrook',
+                    count: 4
+                }, {
+                    student: 'Tommy Ruz',
+                    count: 2
+                }, {
+                    student: 'Brittany Jones',
+                    count: 0
+                }, {
+                    student: 'Mary Salter',
+                    count: 1
+                }],
+                name: 'Exercises Missed'
+            }
 
-stack(dataset);
+            ],
+            series = dataset.map(function (d) {
+                return d.name;
+            }),
+            dataset = dataset.map(function (d) {
+                return d.data.map(function (o, i) {
+                    // Structure it so that your numeric
+                    // axis (the stacked amount) is y
+                    return {
+                        y: o.count,
+                        x: o.student
+                    };
+                });
+            }),
+            stack = d3.layout.stack();
 
-var dataset = dataset.map(function (group) {
-    return group.map(function (d) {
-        // Invert the x and y values, and y0 becomes x0
-        return {
-            x: d.y,
-            y: d.x,
-            x0: d.y0
-        };
-    });
-}),
-    svg = d3.select('body').selectAll('#studentchart')
-        .append('svg')
-        .attr('width', width + margins.left + margins.right + legendPanel.width)
-        .attr('height', height + margins.top + margins.bottom)
-        .append('g')
-        .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')'),
-    xMax = d3.max(dataset, function (group) {
-        return d3.max(group, function (d) {
-            return d.x + d.x0;
-        });
-    }),
-    xScale = d3.scale.linear()
-        .domain([0, xMax])
-        .range([0, width]),
-    students = dataset[0].map(function (d) {
-        return d.y;
-    }),
-    _ = console.log(students),
-    yScale = d3.scale.ordinal()
-        .domain(students)
-        .rangeRoundBands([0, height], .1),
-    xAxis = d3.svg.axis()
-        .scale(xScale)
-        .orient('bottom'),
-    yAxis = d3.svg.axis()
-        .scale(yScale)
-        .orient('left'),
-    colors = ['#009DD9','#00CC66','#CCCC33'],//d3.scale.category10(),
-    groups = svg.selectAll('g')
-        .data(dataset)
-        .enter()
-        .append('g')
-        .style('fill', function (d, i) {
-        return colors[i];
-    }),
-    rects = groups.selectAll('rect')
-        .data(function (d) {
-        return d;
-    })
-        .enter()
-        .append('rect')
-        .attr('x', function (d) {
-        return xScale(d.x0);
-    })
-        .attr('y', function (d, i) {
-        return yScale(d.y);
-    })
-        .attr('height', function (d) {
-        return yScale.rangeBand();
-    })
-        .attr('width', function (d) {
-        return xScale(d.x);
-    })
-        .on('mouseover', function (d) {
-        var xPos = parseFloat(d3.select(this).attr('x')) / 2 + width / 2;
-        var yPos = parseFloat(d3.select(this).attr('y')) + yScale.rangeBand() / 2;
+        stack(dataset);
 
-        d3.select('#tooltip')
-            .style('left', xPos + 'px')
-            .style('top', yPos + 'px')
-            .select('#value')
-            .text(d.x);
+        var dataset = dataset.map(function (group) {
+            return group.map(function (d) {
+                // Invert the x and y values, and y0 becomes x0
+                return {
+                    x: d.y,
+                    y: d.x,
+                    x0: d.y0
+                };
+            });
+        }),
+            svg = d3.select('body').selectAll('#studentchart')
+                .append('svg')
+                .attr('width', width + margins.left + margins.right)
+                .attr('height', height + margins.top + margins.bottom)
+                .append('g')
+                .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')'),
+            xMax = d3.max(dataset, function (group) {
+                return d3.max(group, function (d) {
+                    return d.x + d.x0;
+                });
+            }),
+            xScale = d3.scale.linear()
+                .domain([0, xMax])
+                .range([0, width]),
+            students = dataset[0].map(function (d) {
+                return d.y;
+            }),
+            _ = console.log(students),
+            yScale = d3.scale.ordinal()
+                .domain(students)
+                .rangeRoundBands([0, height], .1),
+            xAxis = d3.svg.axis()
+                .scale(xScale)
+                .orient('bottom'),
+            yAxis = d3.svg.axis()
+                .scale(yScale)
+                .orient('left'),
+            colors = ['#009DD9','#00CC66','#CCCC33'],//d3.scale.category10(),
+            groups = svg.selectAll('g')
+                .data(dataset)
+                .enter()
+                .append('g')
+                .style('fill', function (d, i) {
+                return colors[i];
+            }),
+            rects = groups.selectAll('rect')
+                .data(function (d) {
+                return d;
+            })
+                .enter()
+                .append('rect')
+                .attr('x', function (d) {
+                return xScale(d.x0);
+            })
+                .attr('y', function (d, i) {
+                return yScale(d.y);
+            })
+                .attr('height', function (d) {
+                return yScale.rangeBand()-4;
+            })
+                .attr('width', function (d) {
+                return xScale(d.x);
+            })
+                .on('mouseover', function (d) {
+                var xPos = parseFloat(d3.select(this).attr('x'));/// 2 + width / 2;
+                var yPos = parseFloat(d3.select(this).attr('y'));// + yScale.rangeBand() / 2;
 
-        d3.select('#tooltip').classed('hidden', false);
-    })
-        .on('mouseout', function () {
-        d3.select('#tooltip').classed('hidden', true);
-    })
+                d3.select('#dash-chart-tooltip')
+                    .style('left', xPos + margins.left + 'px')
+                    .style('top', yPos + margins.top + 'px')
+                    .select('#value')
+                    .text(d.x);
 
-    /*svg.append('g')
-        .attr('class', 'axis')
-        .attr('transform', 'translate(0,' + height + ')')
-        .call(xAxis);*/
+                d3.select('#dash-chart-tooltip').attr('hidden', null);
+            })
+                .on('mouseout', function () {
+                d3.select('#dash-chart-tooltip').attr('hidden', true);
+            })
 
-svg.append('g')
-    .attr('class', 'axis')
-    .call(yAxis);
+            /*svg.append('g')
+                .attr('class', 'axis')
+                .attr('transform', 'translate(0,' + height + ')')
+                .call(xAxis);*/
 
-series.forEach(function (s, i) {
-    svg.append('text')
-        .attr('fill', 'black')
-        .attr('x', width + margins.left + 25)
-        .attr('y', i * 25 + 18)
-        .text(s);
-    svg.append('rect')
-        .attr('fill', colors[i])
-        .attr('width', 15)
-        .attr('height', 15)
-        .attr('x', width + margins.left + 5)
-        .attr('y', i * 25 + 5);
-});
+        svg.append('g')
+            .attr('class', 'axis')
+            .call(yAxis);
+    }
+
+    function exerciseAttempts() {
+        var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 500 - margin.left - margin.right,
+    height = 150 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10);
+
+var svg = d3.select("#chart-exercise-attempts").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var data = [{
+        attempts: '1',
+        frequency: 8
+    },{
+        attempts: '2',
+        frequency: 3
+    },{
+        attempts: '3',
+        frequency: 2
+    },{
+        attempts: '4',
+        frequency: 3
+    },{
+        attempts: '5+',
+        frequency: 4
+    }];
+
+
+
+x.domain(data.map(function(d) { return d.attempts; }));
+  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("# Students");
+
+  svg.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.attempts); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.frequency); })
+      .attr("height", function(d) { return height - y(d.frequency); });
+
+function type(d) {
+  d.frequency = +d.frequency;
+  return d;
+}
+
+    }
+
+    function donutChart(data, chartId){
+        var width = 100,
+            height = 100,
+            radius = Math.min(width, height) / 2;
+
+        var color = d3.scale.ordinal()
+            .range(["#CCCCCC","#CC3300", "#CCCC33", "#00CC66"]);
+
+        var arc = d3.svg.arc()
+            .outerRadius(radius - 5)
+            .innerRadius(radius - 25);
+
+        var pie = d3.layout.pie()
+            .sort(null)
+            .value(function(d) { return d.count; });
+
+        var svg = d3.select(chartId).append("svg")
+            .attr("width", width)
+            .attr("height", height)
+          .append("g")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+        var g = svg.selectAll(".arc")
+            .data(pie(data))
+            .enter().append("g")
+              .attr("class", "arc");
+
+        g.append("path")
+            .attr("d", arc)
+            .style("fill", function(d) { return color(d.data.type); })
+            .on('mouseover', function (d) {
+                var xPos = parseFloat(d3.select(this).attr('x'));/// 2 + width / 2;
+                var yPos = parseFloat(d3.select(this).attr('y'));// + yScale.rangeBand() / 2;
+console.log(d);
+                d3.select('#dash-chart-tooltip')
+                    .style('left', xPos + 'px')
+                    .style('top', yPos + 'px')
+                    .select('#value')
+                    .text(d.value);
+                });
+
+        g.append("text")
+            .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+            .attr("dy", ".35em")
+            .text(function(d) { return d.data.count; });
+
+        function type(d) {
+          d.count = +d.count;
+          return d;
+        }
+
+    }
+}
+
