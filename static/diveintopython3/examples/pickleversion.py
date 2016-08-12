592af92bef716f9596018bc2796cafff5a814277
Brad Miller <bonelake@mac.com>
2016-08-11 19:02:49 -0500

    remove diveintopython from static

--- static/diveintopython3/examples/pickleversion.py
+++
@@ -1,8 +0,0 @@
-import pickletools
-
-def protocol_version(file_object):
-    maxproto = -1
-    for opcode, arg, pos in pickletools.genops(file_object):
-        maxproto = max(maxproto, opcode.proto)
-    return maxproto
-
