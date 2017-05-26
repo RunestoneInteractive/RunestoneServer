fc47530bf0963dc03673d21e5387c6f747bebccc
Brad Miller <bonelake@mac.com>
2017-05-26 09:06:41 -0500

    make page load a bit more real

--- scripts/loadtest.py
+++ scripts/loadtest.py
@@ -1,4 +1,6 @@
 from locust import HttpLocust, TaskSet, task
+from lxml import html
+import requests as rq
 
 class SimpleTests(TaskSet):
 
@@ -8,7 +10,8 @@ class SimpleTests(TaskSet):
     
     @task
     def home(self):
-        self.client.get("/runestone/static/thinkcspy/index.html", verify=False)
+        self.client.get("/runestone/static/thinkcspy/PythonTurtle/TheforLoop.html", verify=False)
+        self.getAllAssets()
 
     @task
     def logrun(self):
@@ -16,8 +19,24 @@ class SimpleTests(TaskSet):
             {'event': 'activecode', 'act': 'edit', 'div_id': 'logtest'},
             verify=False)
 
+    @task
+    def dynamicpage(self):
+        self.r = self.client.get("/runestone/course/serve/thinkcspy/PythonTurtle/TheforLoop.html", verify=False)
+        self.getAllAssets()
+
+    def getAllAssets(self):
+        tree = html.fromstring(self.r.text)
+        csslinks = tree.xpath('//link/@href')
+        jslinks = tree.xpath('//script/@src')
+        base = self.r.url[:self.r.url.rfind('/')+1]
+        for l in csslinks:
+            rq.get(base + l, verify=False)
+
+        for l in jslinks:
+            rq.get(base + l, verify=False)
+
 class WebsiteUser(HttpLocust):
     task_set = SimpleTests
     min_wait = 5000
     max_wait = 15000
-
+    host = "https://rsvm"
