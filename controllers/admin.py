d8fde024f7a878a13cf110c5fa7d2f9951db9e39
conzty01 <conzty01@luther.edu>
2017-07-06 09:00:27 -0500

    Fixed issue with missing 'course_id' when rebuilding a course

--- controllers/admin.py
+++ controllers/admin.py
@@ -352,7 +352,7 @@ def admin():
 
     return dict(sectionInfo=sectionsList, startDate=date.isoformat(), coursename=auth.user.course_name,
                 instructors=instructordict, students=studentdict, confirm=False,
-                task_name=uuid, course_url=course_url)
+                task_name=uuid, course_url=course_url, course_id=auth.user.course_id)
 
 @auth.requires(lambda: verifyInstructorStatus(auth.user.course_name, auth.user), requires_login=True)
 def course_students():
