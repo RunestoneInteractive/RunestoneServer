cc05d405a3b38a4d85e91689bed0978059ac89b7
conzty01 <conzty01@luther.edu>
2017-07-07 16:30:43 -0500

    Reseting timed exams only add one row to database per reset

--- controllers/ajax.py
+++ controllers/ajax.py
@@ -63,15 +63,16 @@ def hsblog():    # Human Subjects Board Log
                 logger.debug('correct {} incorrect {} skipped {} time {} reset {}'.format(request.vars.correct, request.vars.incorrect, request.vars.skipped, request.vars.time, request.vars.reset))
                 logger.debug('Error: {}'.format(e.message))
 
-        try:
-            db.timed_exam.insert(sid=sid, course_name=course, correct=int(request.vars.correct),
-                             incorrect=int(request.vars.incorrect), skipped=int(request.vars.skipped),
-                             time_taken=int(tt), timestamp=ts,
-                             div_id=div_id)
-        except Exception as e:
-            logger.debug('failed to insert a timed exam record for {} in {} : {}'.format(sid, course, div_id))
-            logger.debug('correct {} incorrect {} skipped {} time {}'.format(request.vars.correct, request.vars.incorrect, request.vars.skipped, request.vars.time))
-            logger.debug('Error: {}'.format(e.message))
+        else:
+            try:
+                db.timed_exam.insert(sid=sid, course_name=course, correct=int(request.vars.correct),
+                                 incorrect=int(request.vars.incorrect), skipped=int(request.vars.skipped),
+                                 time_taken=int(tt), timestamp=ts,
+                                 div_id=div_id)
+            except Exception as e:
+                logger.debug('failed to insert a timed exam record for {} in {} : {}'.format(sid, course, div_id))
+                logger.debug('correct {} incorrect {} skipped {} time {}'.format(request.vars.correct, request.vars.incorrect, request.vars.skipped, request.vars.time))
+                logger.debug('Error: {}'.format(e.message))
 
     if event == 'mChoice' and auth.user:
         # has user already submitted a correct answer for this question?
