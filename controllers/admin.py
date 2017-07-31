d4127872a64f862560a42c2440de407f229594b8
Brad Miller <bonelake@mac.com>
2017-07-31 14:59:14 -0500

    Keep page questions out of the problem tree picker.

--- controllers/admin.py
+++ controllers/admin.py
@@ -1003,6 +1003,7 @@ def _get_toc_and_questions():
                 questions_query = db((db.courses.course_name == auth.user.course_name) & \
                                      (db.questions.base_course == db.courses.base_course) & \
                                   (db.questions.chapter == ch.chapters.chapter_label) & \
+                                  (db.questions.question_type <> 'page') & \
                                   (db.questions.subchapter == sub_ch.sub_chapter_label)).select()
                 for question in questions_query:
                     q_info = dict(
