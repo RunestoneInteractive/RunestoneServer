f24caf83aabd0d1477831a2e526e6586cc8cbbc8
Iman <oneweb@umich.edu>
2017-12-29 11:15:47 -0500

    Created the model user_topic_practice_log to take log of users' interactions with the spaced repetition system.

--- models/practice.py
+++ models/practice.py
@@ -1,9 +1,24 @@
 db.define_table('user_topic_practice',
                 Field('user_id', db.auth_user),
-                Field('chapter_name','string'),
+                Field('course_name','string'),
+                Field('chapter_label','string'),
                 Field('sub_chapter_label','string'),
                 Field('question_name','string'),
                 Field('i_interval', type='integer', notnull=True),
                 Field('e_factor', type='double', notnull=True),
                 Field('last_practice', type='datetime'),
                 migrate='runestone_spacing.table')
+
+
+db.define_table('user_topic_practice_log',
+                Field('user_id', db.auth_user),
+                Field('course_name','string'),
+                Field('chapter_label','string'),
+                Field('sub_chapter_label','string'),
+                Field('question_name','string'),
+                Field('i_interval', type='integer', notnull=True),
+                Field('e_factor', type='double', notnull=True),
+                Field('trials_num', type='integer', notnull=True),
+                Field('start_practice', type='datetime'),
+                Field('end_practice', type='datetime'),
+                migrate='runestone_spacing_log.table')
