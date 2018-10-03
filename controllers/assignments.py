2ebadee32772b6de13efe91f7a2692d0a0030f51
Iman <oneweb@umich.edu>
2018-10-03 08:14:07 -0400

    I learned that when students under the blocking condition answer something wrong multiple times, it becomes too difficult for them to pass it and the system asks them the same question many times (because most subchapters have only one question). To solve this issue, I changed the blocking formula.

--- controllers/assignments.py
+++ controllers/assignments.py
@@ -211,7 +211,7 @@ def _get_practice_data(user, timezoneoffset):
                 available_flashcards_num = len(presentable_flashcards)
             else:
                 # Select only those that are not mastered yet.
-                presentable_flashcards = [f for f in flashcards if (f.e_factor <= 2.5 and f.q != -1)]
+                presentable_flashcards = [f for f in flashcards if (f.e_factor * f.q < 10 and f.q != -1)]
                 available_flashcards_num = len(presentable_flashcards)
                 if len(presentable_flashcards) > 0:
                     # It's okay to continue with the next chapter if there is no more question in the current chapter
@@ -747,8 +747,14 @@ def practice():
             f_card["mastery_percent"] = int(f_card["remaining_days"])
         else:
             # The maximum q is 5.0 and the minimum e_factor that indicates mastery of the topic is 2.5. `5 * 2.5 = 12.5`
+            # I learned that when students under the blocking condition answer something wrong multiple times,
+            # it becomes too difficult for them to pass it and the system asks them the same question many times
+            # (because most subchapters have only one question). To solve this issue, I changed the blocking formula.
             f_card["mastery_percent"] = int(100 * f_card.user_topic_practice.e_factor *
-                                            f_card.user_topic_practice.q / 12.5)
+                                            f_card.user_topic_practice.q / 10)
+            if f_card["mastery_percent"] > 100:
+                f_card["mastery_percent"] = 100
+
         f_card["mastery_color"] = "danger"
         if f_card["mastery_percent"] >= 75:
             f_card["mastery_color"] = "success"
