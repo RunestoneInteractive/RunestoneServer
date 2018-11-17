a9690adca95dfecf4beee5706101df18b565c826
Paul Resnick <presnick@umich.edu>
2018-11-17 14:59:36 -0500

    change a couple things to depend on academy mode instead of lti_only mode

--- models/scheduler.py
+++ models/scheduler.py
@@ -140,5 +140,5 @@ def makePavement(http_host, rvars, sourcedir, base_course):
     with open(path.join(sourcedir, 'pavement.py'), 'w') as fp:
         fp.write(paver_stuff)
 
-if settings.lti_only !=True:
+if settings.academy_mode != True:
    scheduler = Scheduler(db, migrate='runestone_')
