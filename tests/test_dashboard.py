0500c3cbe64b60fdabcbce548810ecb8d186ceeb
Bradley Miller <bradleymiller@google.com>
2019-04-23 14:54:02 -0500

    use six.StringIO

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -15,7 +15,7 @@ from bs4 import BeautifulSoup
 
 from gluon.globals import Request, Session
 from gluon.tools import Auth
-from io import StringIO
+from six import StringIO
 
 # bring in the ajax controllers
 
