bc839ae6694aeea0cf846d28db89c1855c3b9ad9
Brad Miller <bmiller@dev.runestoneinteractive.org>
2019-06-13 16:48:30 +0000

    test chooseAssignment

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -11,3 +11,13 @@ def test_add_assignment(test_assignment, test_client, test_user_1, runestone_db_
     assert str(res.duedate.date()) == str(my_ass.due.date())
     my_ass.autograde()
     my_ass.calculate_totals()
+
+
+def test_choose_assignment(test_assignment, test_client, test_user_1, runestone_db_tools):
+    my_ass = test_assignment('test_assignment', 'test_course_1')
+    my_ass.addq_to_assignment(div_id='subc_b_fitb',points=10)
+    my_ass.description = 'Test Assignment Description'
+    my_ass.make_visible()
+    test_user_1.login()
+    test_client.validate('assignments/chooseAssignment.html','Test Assignment Description')
+
