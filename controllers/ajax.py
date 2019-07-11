285dfd4095f56bc42a1526dba9fbd4600b6eb9fc
Scott Hill <physhill@gmail.com>
2019-07-10 21:23:32 -0400

    Wrapped stdout and stderr in str() to prevent potential errors

--- controllers/ajax.py
+++ controllers/ajax.py
@@ -994,7 +994,7 @@ def preview_question():
         # If there was an error, return stdout and stderr from the build.
         if popen_obj.returncode != 0:
             return json.dumps('Error: Runestone build failed:\n\n' +
-                              stdout + '\n' + stderr)
+                              str(stdout) + '\n' + str(stderr))
 
         with open('applications/{}/build/preview/build/preview/index.html'.format(request.application), 'r', encoding='utf-8') as ixf:
             src = ixf.read()
