ae057185b01116bc24c5ca112b1024ee33b285b5
Scott Hill <physhill@gmail.com>
2019-07-31 09:49:07 -0400

    Added Toggle Visibility button to Student Activity (doesn't work)

--- models/db.py
+++ models/db.py
@@ -292,3 +292,13 @@ def check_for_donate_or_build(field_dict,id_of_insert):
 
 if 'auth_user' in db:
     db.auth_user._after_insert.append(check_for_donate_or_build)
+
+auth.settings.actions_disabled.append('register')
+
+auth.settings.actions_disabled.append('register')
+
+auth.settings.actions_disabled.append('register')
+
+auth.settings.actions_disabled.append('register')
+
+auth.settings.actions_disabled.append('register')
