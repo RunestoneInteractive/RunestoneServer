56ac224405b5f08944a9ac3989564ff468167984
Brad Miller <brad@runestoneinteractive.com>
2019-08-29 16:03:56 +0000

    fix format check to work without -k

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -1,9 +1,16 @@
 import sys
+import os
 from .ci_utils import xqt, pushd
 
 
 def tests_black_format_check():
-    with pushd("../"):
+    cwd = os.getcwd()
+    print("CWD = ", cwd)
+    if cwd.endswith("tests"):
+        pd = "../"
+    else:
+        pd = "applications/runestone"
+    with pushd(pd):
         rc = xqt(
             "{} -m black --check controllers models modules tests --exclude 1.py".format(
                 sys.executable
