RunestoneInteractive/RunestoneServer

commit bacb396faa524a600ecdfe5882eec9c3e60a2b1a

Fix: Report profile output at end of test run.

tests/ci_utils.py
@@ -10,7 +10,7 @@
 # Standard library
 # ----------------
 from __future__ import print_function
-from subprocess import check_call
+from subprocess import run
 import sys
 import os
 import os.path
@@ -24,19 +24,22 @@ is_darwin = sys.platform == 'darwin'
 
 # Copied from https://docs.python.org/3.5/library/platform.html#cross-platform.
 is_64bits = sys.maxsize > 2**32
-#
+
+
 # Support code
 # ============
 # xqt
 # ---
 # Pronounced "execute": provides a simple way to execute a system command.
 def xqt(
-  # Commands to run. For example, ``'foo -param firstArg secondArg', 'bar |
-  # grep alpha'``.
-  *cmds,
-  # Optional keyword arguments to pass on to `subprocess.check_call <https://docs.python.org/3/library/subprocess.html#subprocess.check_call>`_.
-  **kwargs):
+    # Commands to run. For example, ``'foo -param firstArg secondArg', 'bar |
+    # grep alpha'``.
+    *cmds,
+    # Optional keyword arguments to pass on to `subprocess.check_call <https://docs.python.org/3/library/subprocess.html#subprocess.check_call>`_.
+    **kwargs
+):
 
+    ret = []
     # Although https://docs.python.org/3/library/subprocess.html#subprocess.Popen
     # states, "The only time you need to specify ``shell=True`` on Windows is
     # when the command you wish to execute is built into the shell (e.g.
@@ -55,15 +58,21 @@ def xqt(
         # works. See https://docs.python.org/3/library/subprocess.html#subprocess.Popen.
         executable = ('/bin/bash' if is_linux or is_darwin
                       else None)
-        check_call(_, shell=True, executable=executable, **kwargs)
-#
+        ret.append(run(_, shell=True, capture_output=True,
+                       executable=executable, **kwargs))
+
+    # Return a list only if there were multiple commands to execute.
+    return ret[0] if len(ret) == 1 else ret
+
+
 # pushd
 # -----
 # A context manager for pushd.
 class pushd:
     def __init__(self,
-      # The path to change to upon entering the context manager.
-      path):
+        # The path to change to upon entering the context manager.
+        path
+    ):
 
         self.path = path
 
@@ -76,7 +85,8 @@ class pushd:
         flush_print('popd - returning to {}.'.format(self.cwd))
         os.chdir(self.cwd)
         return False
-#
+
+
 # Common tools
 # ============
 #
@@ -85,13 +95,15 @@ class pushd:
 def chdir(path):
     flush_print('cd ' + path)
     os.chdir(path)
-#
+
+
 # mkdir
 # -----
 def mkdir(path):
     flush_print('mkdir ' + path)
     os.mkdir(path)
-#
+
+
 # flush_print
 # -----------
 # Anything sent to ``print`` won't be printed until Python flushes its buffers,
@@ -102,14 +114,16 @@ def flush_print(*args, **kwargs):
     # Flush both buffers, just in case there's something in ``stdout``.
     sys.stdout.flush()
     sys.stderr.flush()
-#
+
+
 # isfile
 # ------
 def isfile(f):
     _ = os.path.isfile(f)
     flush_print('File {} {}.'.format(f, 'exists' if _ else 'does not exist'))
     return _
-#
+
+
 # isfile
 # ------
 def isdir(f):

tests/conftest.py
@@ -64,6 +64,14 @@ def pytest_addoption(parser):
                      help='Skip W3C validation of web pages.')
 
 
+# Output a coverage report when testing is done. See https://docs.pytest.org/en/latest/reference.html#_pytest.hookspec.pytest_terminal_summary.
+def pytest_terminal_summary(terminalreporter):
+    with pushd('../../..'):
+        cp = xqt('{} -m coverage report'.format(sys.executable), text=True)
+    terminalreporter.write_line(cp.stdout + cp.stderr)
+
+
+
 # Utilities
 # =========
 # A simple data-struct object.
@@ -218,8 +226,6 @@ def web2py_server(runestone_name, web2py_server_address, pytestconfig):
         web2py_server.terminate()
         web2py_scheduler.terminate()
         echo_thread.join()
-
-        xqt('{} -m coverage report'.format(sys.executable))
 
 
 # The name of the Runestone controller. It must be module scoped to allow the ``web2py_server`` to use it.

commit cce05e65a6af4f0232f5e2b58d06171ff75a13a3

Fix: Correct incorrect subprocess.run parameters introduced in bacb396faa524a600ecdfe5882eec9c3e60a2b1a.

tests/ci_utils.py
@@ -10,7 +10,7 @@
 # Standard library
 # ----------------
 from __future__ import print_function
-from subprocess import run, PIPE
+from subprocess import run
 import sys
 import os
 import os.path
@@ -36,7 +36,7 @@ def xqt(
     # Commands to run. For example, ``'foo -param firstArg secondArg', 'bar |
     # grep alpha'``.
     *cmds,
-    # Optional keyword arguments to pass on to `subprocess.check_call <https://docs.python.org/3/library/subprocess.html#subprocess.check_call>`_.
+    # Optional keyword arguments to pass on to `subprocess.run <https://docs.python.org/3/library/subprocess.html#subprocess.run>`_.
     **kwargs
 ):
 
@@ -58,11 +58,7 @@ def xqt(
         # Use bash instead of sh, so that ``source`` and other bash syntax
         # works. See https://docs.python.org/3/library/subprocess.html#subprocess.Popen.
         executable = "/bin/bash" if is_linux or is_darwin else None
-        ret.append(
-            run(
-                _, shell=True, stdout=PIPE, stderr=PIPE, executable=executable, **kwargs
-            )
-        )
+        ret.append(run(_, shell=True, executable=executable, check=True, **kwargs))
 
     # Return a list only if there were multiple commands to execute.
     return ret[0] if len(ret) == 1 else ret

tests/conftest.py
@@ -77,10 +77,19 @@ def pytest_addoption(parser):
 # Output a coverage report when testing is done. See https://docs.pytest.org/en/latest/reference.html#_pytest.hookspec.pytest_terminal_summary.
 def pytest_terminal_summary(terminalreporter):
     with pushd("../../.."):
-        cp = xqt(
-            "{} -m coverage report".format(sys.executable), universal_newlines=True
-        )
-    terminalreporter.write_line(cp.stdout + cp.stderr)
+        try:
+            cp = xqt(
+                "{} -m coverage report".format(sys.executable),
+                # Capture the output from the report.
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                universal_newlines=True,
+            )
+        except subprocess.CalledProcessError as e:
+            res = "Error in coverage report.\n{}".format(e.stdout + e.stderr)
+        else:
+            res = cp.stdout + cp.stderr
+    terminalreporter.write_line(res)
 
 
 # Utilities

tests/test_formatting.py
@@ -11,14 +11,11 @@ def tests_black_format_check():
     else:
         pd = "applications/runestone"
     with pushd(pd):
-        rc = xqt(
+        xqt(
             "{} -m black --check controllers models modules tests --exclude 1.py".format(
                 sys.executable
-            ),
-            universal_newlines=True,
+            )
         )
-        print(rc.stdout + rc.stderr)
-        assert rc.returncode == 0
 
 
 def test_flake8_lint():
@@ -33,10 +30,4 @@ def test_flake8_lint():
         models = os.listdir("models")
         controllers = [x for x in controllers + models if x.endswith(".py")]
         for c in controllers:
-            print(c)
-            rc = xqt(
-                "{} -m flake8 controllers/{}".format(sys.executable, c),
-                universal_newlines=True,
-            )
-            print(rc.stdout + rc.stderr)
-            assert rc.returncode == 0
+            xqt("{} -m flake8 controllers/{}".format(sys.executable, c))