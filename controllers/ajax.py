2a2969039b2041c3cfed9cef39c6a4e9e31d7a74
Brad Miller <brad@runestoneinteractive.com>
2019-12-23 23:52:03 +0000

    black

--- controllers/ajax.py
+++ controllers/ajax.py
@@ -62,9 +62,7 @@ def hsblog():
             )
         sid = auth.user.username
         compareAndUpdateCookieData(sid)
-        setCookie = (
-            True
-        )  # we set our own cookie anyway to eliminate many of the extraneous anonymous
+        setCookie = True  # we set our own cookie anyway to eliminate many of the extraneous anonymous
         # log entries that come from auth timing out even but the user hasn't reloaded
         # the page.
     else:
