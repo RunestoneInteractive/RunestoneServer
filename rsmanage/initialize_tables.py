e3b5c4978e8405e10b5f2102b2446d5462eaa590
Brad Miller <brad@runestoneinteractive.com>
2020-09-07 15:00:04 +0000

    resort indices

--- rsmanage/initialize_tables.py
+++ rsmanage/initialize_tables.py
@@ -86,57 +86,57 @@ if environ.get("WEB2PY_MIGRATE", "") != "fake":
         """alter table grades ADD CONSTRAINT user_assign_unique UNIQUE (auth_user, assignment);""",
         """alter table assignments add constraint unique_assign_names unique (name, course)""",
         """alter table course_attributes add constraint course_attr_unique UNIQUE(course_id, attr);""",
-        ## Indexes; alphabetically by table name
+        ## Indexes; alphabetically by index
+        """CREATE INDEX "course_id_index" on useinfo using btree (course_id);""",
+        """CREATE INDEX "course_name_index" on user_topic_practice_log using btree (course_name);""",
+        """CREATE INDEX "div_id_index" on useinfo using btree (div_id);""",
+        """CREATE INDEX "event_index" on useinfo using btree (event);""",
+        """CREATE INDEX "q_index" on user_topic_practice_log using btree (q);""",
+        """CREATE INDEX "sid_index" on useinfo using btree (sid);""",
+        """CREATE INDEX "timestamp_idx" on useinfo using btree ("timestamp");""",
+        """CREATE INDEX "user_id_index" on user_topic_practice_log using btree (user_id);""",
         """CREATE INDEX assign_course_idx ON assignments USING btree (course)""",  # New
-        """CREATE UNIQUE INDEX unique_user ON auth_user USING btree (username)""",  # New
-        """CREATE INDEX chapters_course_id_idx ON chapters USING btree (course_id);""",
-        """create index code_sid_idx on code using btree(sid)""",
-        """create index code_acid_idx on code using btree(acid)""",
-        """create index code_course_id_idx on code using btree(course_id)""",
-        """create index code_timestamp_idx on code using btree(timestamp)""",
         """CREATE INDEX c_i_idx ON course_instructor USING btree (course, instructor)""",  # New
-        """CREATE UNIQUE INDEX courses_course_name_key ON courses USING btree (course_name)""",  # New
-        """CREATE UNIQUE INDEX user_assign_unique_idx ON grades USING btree (auth_user, assignment)""",  # New
+        """CREATE INDEX chap_label_idx on sub_chapters using btree(sub_chapter_label);""",
+        """CREATE INDEX chap_subchap_idx ON questions USING btree (chapter, subchapter)""",  # New
+        """CREATE INDEX chapters_course_id_idx ON chapters USING btree (course_id);""",
+        """CREATE INDEX code_acid_idx on code using btree(acid)""",
+        """CREATE INDEX code_course_id_idx on code using btree(course_id)""",
+        """CREATE INDEX code_sid_idx on code using btree(sid)""",
+        """CREATE INDEX code_timestamp_idx on code using btree(timestamp)""",
+        """CREATE INDEX course_attr_idx ON course_attributes USING btree(course_id, attr);""",
         """CREATE INDEX mchoice_answers_course_name_idx ON mchoice_answers USING btree (course_name);""",
         """CREATE INDEX mchoice_answers_div_id_idx ON mchoice_answers USING btree (div_id);""",
         """CREATE INDEX mchoice_answers_sid_idx ON mchoice_answers USING btree (sid);""",
-        """create index mult_scd_idx on mchoice_answers (div_id, course_name, sid)""",
+        """CREATE INDEX mult_scd_idx on mchoice_answers (div_id, course_name, sid)""",
         """CREATE INDEX parsons_answers_course_name_idx ON parsons_answers USING btree (course_name)""",  # New
         """CREATE INDEX parsons_answers_div_id_idx ON parsons_answers USING btree (div_id)""",  # New
         """CREATE INDEX parsons_answers_sid_idx ON parsons_answers USING btree (sid)""",  # New
         """CREATE INDEX parsons_scd_idx ON parsons_answers USING btree (div_id, course_name, sid)""",  # New
-        """create index question_grades_key on question_grades (div_id, course_name, sid)""",
-        """CREATE INDEX chap_subchap_idx ON questions USING btree (chapter, subchapter)""",  # New
         """CREATE INDEX q_bc_idx ON questions USING btree (base_course) """,  # New
+        """CREATE INDEX question_grades_key on question_grades (div_id, course_name, sid)""",
         """CREATE INDEX questions_chapter_idx ON questions USING btree (chapter);""",
         """CREATE INDEX questions_name_idx ON questions USING btree (name);""",
-        """CREATE INDEX subchap_idx ON questions USING btree (subchapter)""",  # New
-        """CREATE UNIQUE INDEX scheduler_task_uuid_key ON scheduler_task USING btree (uuid)""",  # New
-        """CREATE UNIQUE INDEX scheduler_worker_worker_name_key ON scheduler_worker USING btree (worker_name)""",  # New
+        """CREATE INDEX sid_divid_idx ON useinfo USING btree(sid, div_id)""",
         """CREATE INDEX source_code_acid_idx ON source_code USING btree (acid)""",  # New
         """CREATE INDEX source_code_course_id_idx ON source_code USING btree (course_id)""",  # New
         """CREATE INDEX sub_chapters_chapter_id_idx ON sub_chapters USING btree (chapter_id);""",
-        """CREATE UNIQUE INDEX tags_tag_name_key ON tags USING btree (tag_name)""",  # New
-        """create index "course_id_index" on useinfo using btree (course_id);""",
-        """create index "div_id_index" on useinfo using btree (div_id);""",
-        """create index "event_index" on useinfo using btree (event);""",
-        """create index "sid_index" on useinfo using btree (sid);""",
-        """create index "timestamp_idx" on useinfo using btree ("timestamp");""",
+        """CREATE INDEX subchap_idx ON questions USING btree (subchapter)""",  # New
+        """CREATE INDEX unittest_answers_course_name_idx ON unittest_answers USING btree (course_name);""",
+        """CREATE INDEX unittest_answers_div_id_idx ON unittest_answers USING btree (div_id);""",
+        """CREATE INDEX unittest_answers_sid_idx ON unittest_answers USING btree (sid);""",
         """CREATE INDEX us_cid_idx ON public.user_state USING btree (course_id)""",  # New
         """CREATE INDEX us_sid_idx ON public.user_state USING btree (user_id)""",  # New
         """CREATE INDEX user_sub_chapter_progress_chapter_id_idx ON user_sub_chapter_progress USING btree (chapter_id);""",
         """CREATE INDEX user_sub_chapter_progress_user_id_idx ON user_sub_chapter_progress USING btree (user_id)""",  # New
-        """CREATE UNIQUE INDEX selector_sid_unique ON selected_questions USING btree (selector_id, sid)""",
-        """CREATE INDEX unittest_answers_course_name_idx ON unittest_answers USING btree (course_name);""",
-        """CREATE INDEX unittest_answers_div_id_idx ON unittest_answers USING btree (div_id);""",
-        """CREATE INDEX unittest_answers_sid_idx ON unittest_answers USING btree (sid);""",
-        """CREATE INDEX chap_label_idx on sub_chapters using btree(sub_chapter_label);""",
+        """CREATE UNIQUE INDEX courses_course_name_key ON courses USING btree (course_name)""",  # New
         """CREATE UNIQUE INDEX q_comp_unique ON competency USING btree (question, competency)""",
-        """CREATE INDEX sid_divid_idx ON useinfo USING btree(sid, div_id)""",
-        """create index "course_name_index" on user_topic_practice_log using btree (course_name);""",
-        """create index "user_id_index" on user_topic_practice_log using btree (user_id);""",
-        """create index "q_index" on user_topic_practice_log using btree (q);""",
-        """CREATE INDEX course_attr_idx ON course_attributes USING btree(course_id, attr);""",
+        """CREATE UNIQUE INDEX scheduler_task_uuid_key ON scheduler_task USING btree (uuid)""",  # New
+        """CREATE UNIQUE INDEX scheduler_worker_worker_name_key ON scheduler_worker USING btree (worker_name)""",  # New
+        """CREATE UNIQUE INDEX selector_sid_unique ON selected_questions USING btree (selector_id, sid)""",
+        """CREATE UNIQUE INDEX tags_tag_name_key ON tags USING btree (tag_name)""",  # New
+        """CREATE UNIQUE INDEX unique_user ON auth_user USING btree (username)""",  # New
+        """CREATE UNIQUE INDEX user_assign_unique_idx ON grades USING btree (auth_user, assignment)""",  # New
     ]
 
     for cmd in sql_commands:
