c0138c6827448de5d47a9702a88b8416f396dbb7
Brad Miller <brad@runestoneinteractive.com>
2020-09-11 19:15:45 +0000

    set domain for session in production

--- models/db.py
+++ models/db.py
@@ -82,6 +82,9 @@ if "https" in settings.server_type and settings.lti_iframes is True:
     session.secure()
     session.samesite("None")
 
+if settings.session_domain:
+    response.cookies["session_id_runestone"]["domain"] = settings.session_domain
+
 ## by default give a view/generic.extension to all actions from localhost
 ## none otherwise. a pattern can be 'controller/function.extension'
 response.generic_patterns = ["*"] if request.is_local else []
