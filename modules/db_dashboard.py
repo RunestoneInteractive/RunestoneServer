30c597eb2b3cb2ffd8fb5d4df887d5b1bed6aa86
Adam Spontarelli <spontarelliam@gmail.com>
2020-10-13 08:34:29 -0400

    changed int conversion to proper rounding operation

--- modules/db_dashboard.py
+++ modules/db_dashboard.py
@@ -662,7 +662,7 @@ class DashboardDataAnalyzer(object):
                                 userEntry["auth_user"]["username"] == username
                             ):  # If this is the student we are looking for
                                 self.grades[assign["name"]]["score"] = this_score
-                                self.grades[assign["name"]]["pct"] = int(
+                                self.grades[assign["name"]]["pct"] = round(
                                     100 * this_score / assign["points"]
                                 )
                     if "score" not in self.grades[assign["name"]]:
