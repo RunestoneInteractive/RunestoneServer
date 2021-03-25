afcab3dc122aaaafe67958cbb897f8d844a2ec15
vqum <40499850+vqum@users.noreply.github.com>
2021-03-25 01:07:01 -0400

    move total points javascript into admin.js

--- static/js/admin.js
+++ static/js/admin.js
@@ -2356,3 +2356,16 @@ function copyElementToClipboard(elid) {
     document.execCommand("copy");
     document.body.removeChild(el);
 }
+
+$(document).ready(function() {
+    var totalPointsDiv = document.getElementById('totalPoints');
+    var totalPointsCopy = document.getElementById('totalPointsCopy');
+    var observer = new MutationObserver(function() {
+        totalPointsCopy.innerHTML = totalPointsDiv.innerHTML;
+    });
+    observer.observe(totalPointsDiv, {
+        attributes: true,
+        childList: true,
+        characterData: true
+    });
+});
