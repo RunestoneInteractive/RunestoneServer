48b9d1215470e0c6a357e5fcd64a06e026ae8abd
Bryar Frank <frankb@berea.edu>
2021-07-05 17:33:02 -0400

    "renames variables and removes some extrainious variables"

--- static/js/markcomplete.js
+++ static/js/markcomplete.js
@@ -1,12 +1,11 @@
-function markComplete(assignment_id, student_id, is_submit) {
+function markComplete(assignment_id, student_id) {
     jQuery.ajax({
         url: eBookConfig.app + '/assignments/update_submit',
         type: "POST",
         dataType: "JSON",
         data: {
             assignment_id: assignment_id,
-            student_id:student_id,
-            is_submit:is_submit,
+            student_id:student_id
         },
         success: function (retdata) {
             window.location.reload(true);
