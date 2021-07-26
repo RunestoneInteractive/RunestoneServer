5cc7db211f9c69d07e971743197616db5557411f
Brad Miller <bonelake@mac.com>
2021-07-26 08:58:25 -0500

    Prototype of autoupdated graphs

--- controllers/peer.py
+++ controllers/peer.py
@@ -10,17 +10,21 @@
 #
 # Standard library
 # ----------------
-
+import os
+import logging
 
 # Third Party
 # -----------
 import pandas as pd
 import altair as alt
 
+logger = logging.getLogger(settings.logger)
+logger.setLevel(settings.log_level)
+
 
 def dashboard():
 
-    current_question = db(db.questions.name == "question1_2_3").select().first()
+    current_question = db(db.questions.name == "question1_1").select().first()
     return dict(
         course_id=auth.user.course_name,
         course=get_course_row(db.courses.ALL),
@@ -28,7 +32,51 @@ def dashboard():
     )
 
 
+@auth.requires(
+    lambda: verifyInstructorStatus(auth.user.course_id, auth.user),
+    requires_login=True,
+)
 def chartdata():
     response.headers["content-type"] = "application/json"
     div_id = request.vars.div_id
     qnum = request.vars.answer_num
+    course_name = auth.user.course_name
+    dburl = settings.database_uri.replace("postgres://", "postgresql://")
+    logger.debug(f"divid = {div_id}")
+    df = pd.read_sql_query(
+        f"""
+    WITH first_answer AS (
+        SELECT
+            *,
+            ROW_NUMBER() OVER (
+                PARTITION BY sid
+                ORDER BY
+                    id
+            ) AS rn
+        FROM
+            mchoice_answers
+        WHERE
+            div_id = '{div_id}'
+            AND course_name = '{course_name}'
+    )
+    SELECT
+        *
+    FROM
+        first_answer
+    WHERE
+        rn <= 2
+    ORDER BY
+        sid
+    limit 4000    
+    """,
+        dburl,
+    )
+
+    df = df.dropna(subset=["answer"])
+    logger.debug(df.head())
+    df["answer"] = df.answer.astype("int64")
+    df["letter"] = df.answer.map(lambda x: chr(65 + x))
+    c = alt.Chart(df[df.rn == 1]).mark_bar().encode(x="letter", y="count()")
+    d = alt.Chart(df[df.rn == 2]).mark_bar().encode(x="letter", y="count()")
+
+    return alt.vconcat(c, d).to_json()
