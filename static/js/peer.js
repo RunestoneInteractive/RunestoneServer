f6c69d3b14e2cbcfca4313ea52123d9fdc6640d9
Brad Miller <bonelake@mac.com>
2021-08-02 17:30:08 -0500

    Peer management progress
    * add stop the vote
    * add start the vote
    * add hide/show graph

---
+++ static/js/peer.js
@@ -0,0 +1,76 @@
+var ws = null;
+function connect(event) {
+    ws = new WebSocket(`ws://localhost:8080/chat/${user}/ws`);
+    ws.onmessage = function (event) {
+        var messages = document.getElementById('messages')
+        var message = document.createElement('li')
+        let mess = JSON.parse(event.data);
+        if (mess.type === "text") {
+            var content = document.createTextNode(mess.message)
+            message.appendChild(content)
+            messages.appendChild(message)
+        } else if (mess.type === "control") {
+            switch (mess.message) {
+                // This will be some kind of control message for the page
+                case "countDownAndStop":
+                    let messarea = document.getElementById("imessage");
+                    let count = 10;
+                    let itimerid = setInterval(function () {
+                        if (count > 0) {
+                            messarea.innerHTML = `<h3>Finish Up only ${count} seconds remaining</h3>`;
+                            count = count - 1;
+                        } else {
+                            messarea.innerHTML = `<h3>Time is up</h3>`;
+                            window.mcList[currentQuestion].submitButton.disabled = true;
+                            clearInterval(itimerid);
+                        }
+                    }, 1000);
+                    break;
+                case "enableVote":
+                    window.mcList[currentQuestion].submitButton.disabled = false;
+                    let messarea = document.getElementById("imessage");
+                    messarea.innerHTML = `<h3>Time to make you 2nd vote</h3>`
+                    break;
+                default:
+                    console.log("unknown control message");
+            }
+        }
+    }
+}
+// Send a message to the websocket server
+// the server can then broadcast the message or send it to a
+// specific user
+function sendMessage(event) {
+    var input = document.getElementById("messageText")
+    let mess = { type: "text", from: `${user}`, message: input.value, broadcast: false };
+
+    ws.send(JSON.stringify(mess))
+    var messages = document.getElementById('messages')
+    var message = document.createElement('li')
+    var content = document.createTextNode(input.value)
+    message.appendChild(content)
+    messages.appendChild(message)
+    input.value = ''
+    // not needed for onclick event.preventDefault()
+}
+
+function warnAndStopVote(event) {
+    let mess = {
+        type: "control",
+        from: `${user}`,
+        message: "countDownAndStop",
+        broadcast: true
+    }
+    ws.send(JSON.stringify(mess))
+}
+
+function startVote2(event) {
+    let mess = {
+        type: "control",
+        from: `${user}`,
+        message: "enableVote",
+        broadcast: true
+    }
+    ws.send(JSON.stringify(mess));
+
+}
