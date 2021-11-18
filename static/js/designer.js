4920ce6c72bfe24ec6a702811fdfc3bccb35a4b6
Hope Moran <hopemoran@Hopes-MacBook-Air.local>
2021-11-17 21:52:09 -0600

    New UI changes: warnings/alerts set up for edit and delete, hover color of buttons on right side fixed, and draft button blurred

--- static/js/designer.js
+++ static/js/designer.js
@@ -1,7 +1,21 @@
 function validateForm2(form) {
-    for(let num of Object.keys(form)){
-        if (["newBookIdentifier","baseBook","newGithubRepo","githubUser"].includes(form[num].name)){
+    console.log("hellow orld")
+    for (let num of Object.keys(form)) {
+        console.log(form[num].name)
+        if (form[num].name == "changeType") {
+            console.log("test")
+            if (form[num].value == "delete") {
+                console.log("test2")
+                if(!confirm(`Are you sure you want to delete this book? This will also delete your repo.`)) {
+                    alert("test3")
+                    return false;
+                }
+            }
 
+        }
+        if (["newBookIdentifier", "baseBook", "newGithubRepo", "githubUser"].includes(form[num].name)) {
+
+            
             if (form[num].value == "" || form[num].value.indexOf(" ") !== -1 || form[num].value.indexOf("/") > -1) {
                 alert(`Error:  Your ${form[num].name} may not contain spaces or /`)
                 return false;
@@ -14,7 +28,7 @@ function validateForm2(form) {
 
         }
     }
-    form.action="/runestone/designer/book_edit"
+    form.action = "/runestone/designer/book_edit"
     form.submit()
     return true
 }
