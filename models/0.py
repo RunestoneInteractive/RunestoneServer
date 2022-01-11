3b770da4c358eed0314346b8e2697b28e07ae800
bjones1 <bjones@ece.msstate.edu>
2022-01-10 18:04:52 -0600

    Clean: De-duplicate settings in both 0.py and 1.py.prototype.        Put confidential settings in .env.prototype.

--- models/0.py
+++ models/0.py
@@ -22,9 +22,6 @@ settings.author_email = "info@interactivepython.org"
 settings.keywords = ""
 settings.description = ""
 settings.layout_theme = "Default"
-settings.email_server = "smtp.webfaction.com"
-settings.email_sender = "info@interactivepython.org"
-settings.email_login = "sendmail_bnmnetp@web407.webfaction.com:password"
 settings.login_method = "local"
 settings.login_config = ""
 settings.course_id = "devcourse"
@@ -39,20 +36,16 @@ config = environ.get("WEB2PY_CONFIG", "NOT SET")
 
 if config == "production":
     settings.database_uri = environ["DBURL"]
-    # Set these
     settings.STRIPE_PUBLISHABLE_KEY = environ.get("STRIPE_PUBLISHABLE_KEY")
     settings.STRIPE_SECRET_KEY = environ.get("STRIPE_SECRET_KEY")
-    settings.components_version = "4.1.12"
 elif config == "development":
     settings.database_uri = environ.get("DEV_DBURL")
     settings.STRIPE_PUBLISHABLE_KEY = environ.get("STRIPE_DEV_PUBLISHABLE_KEY")
     settings.STRIPE_SECRET_KEY = environ.get("STRIPE_DEV_SECRET_KEY")
-    settings.components_version = "4.1.12"  # str(int(time.time()))
 elif config == "test":
     settings.database_uri = environ.get("TEST_DBURL")
     settings.STRIPE_PUBLISHABLE_KEY = environ.get("STRIPE_TEST_PUBLISHABLE_KEY")
     settings.STRIPE_SECRET_KEY = environ.get("STRIPE_TEST_SECRET_KEY")
-    settings.components_version = str(int(time.time()))
 else:
     print("To configure web2py you should set up both WEB2PY_CONFIG and")
     print("XXX_DBURL values in your environment -- See README for more detail")
@@ -67,8 +60,6 @@ settings.sched_logger = (
     settings.logger
 )  # works for production where sending log to syslog but not for dev.
 settings.log_level = logging.DEBUG
-settings.python_interpreter = sys.executable
 
 settings.pretext_delimiters = ("~._", "_.~")
-settings.lti_interface = True
 settings.num_banners = 0
