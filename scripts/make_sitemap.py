7cac25695f8e2b3f293b498c9520bbd9b8d37b18
Brad Miller <bonelake@mac.com>
2022-08-18 17:04:32 -0500

    Script to install a sitmap.xml in /static
    * This details all of the major html pages for all books to ensure that they are properly indexed.

--- scripts/make_sitemap.py
+++ scripts/make_sitemap.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # /Users/bmiller/Runestone/RunestoneServer/books/csawesome/published/csawesome/MixedFreeResponse/RandomStringChooserParsonsB.html
 # becomes /ns/books/published/
@@ -7,15 +8,17 @@ def path_to_url(p):
     return "https://runestone.academy/ns/books/published" + parts[-1]
 
 
-with open("sitemap.xml", "w") as sm:
+home = os.environ["HOME"]
+
+with open(Path(home, "Runestone/RunestoneServer/static", "sitemap.xml"), "w") as sm:
     sm.write(
-        """
-<?xml version="1.0" encoding="utf-8"?>
+        """<?xml version="1.0" encoding="utf-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     <url><loc>https://runestone.academy</loc></url>
+    <url><loc>https://runestone.academy/runestone/books/index</loc></url>
 """
     )
-    p = Path("/Users/bmiller/Runestone/RunestoneServer/books")
+    p = Path(home, "Runestone/books")
     for i in p.rglob("*.html"):
         if "build" in str(i) or "knowl" in str(i):
             continue
