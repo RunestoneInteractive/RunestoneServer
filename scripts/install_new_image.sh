ebfeee7abe278fec1dcb719c857dd173335fa6f3
Brad Miller <bonelake@mac.com>
2022-10-08 10:35:35 -0500

    Fix: need latest to conform with docker-compose.yml

--- scripts/install_new_image.sh
+++ scripts/install_new_image.sh
@@ -12,12 +12,12 @@ cd /home/bmiller/Runestone/RunestoneServer/production
 
 if [[ $PWD != */production ]]; then
     echo 'you need to be in the production folder for this'
-    exit 
+    exit
 fi
 
 # pull the latest
 git pull
-docker pull registry.digitalocean.com/runestone-registry/production_server:version$1
+docker pull registry.digitalocean.com/runestone-registry/production_server:latest
 
 docker compose stop
 docker compose up -d
